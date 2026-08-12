a1e1d917dc3a0b6189bfb23e127a340130fa5c40
elisinha <elisamcosta0609@gmail.com>
2026-08-12 18:31:46 -0300

    commit da elisinha

---
+++ ensor.cpp sensor.py
@@ -0,0 +1,21 @@
+[1mdiff --git a/sensor.cpp b/sensor.cpp[m
+[1mindex 0a16bed..6abe6ce 100644[m
+[1m--- a/sensor.cpp[m
+[1m+++ b/sensor.cpp[m
+[36m@@ -1,7 +1,7 @@[m
+ #include <iostream>[m
+ [m
+ int main() {[m
+[31m-    double temperatura = 26.5;[m
+[32m+[m[32m    double temperatura = 27;[m[41m[m
+     std::cout << "Sensor ativo: " << temperatura << " C\n";[m
+     return 0;[m
+ }[m
+[1mdiff --git a/sensor.py b/sensor.py[m
+[1mindex 7082be5..abeadbf 100644[m
+[1m--- a/sensor.py[m
+[1m+++ b/sensor.py[m
+[36m@@ -1,2 +1,2 @@[m
+[31m-temperatura = 26.5[m
+[32m+[m[32mtemperatura = 27[m[41m[m
+ print(f"Sensor ativo: {temperatura} C")[m
